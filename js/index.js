94479aa8a40708285c5c4df4bb30d4cd18479163
nacho matallana <nachomatallana@gmail.com>
2024-11-18 19:15:40 -0300

    Entrega final semi

--- js/index.js
+++ js/index.js
@@ -3,67 +3,21 @@ const SPAN_TOTAL = document.getElementById(`precio-total`)
 const SECCION = document.getElementById(`mostrarcart`)
 const UL = document.getElementById(`items-carrito`)
 
-
-/* const PRODUCTOS = [ //creo un array en el cual tiene varios objetos para asi poder acceder a este y filtrar el tipo de producto, encotrar si se encutra el producto, saber si tiene el stock y sumar el precio de los productos.
-    {
-        id: 1,
-        nombre: `jorgito`,
-        precio: 900,
-        tipo: `alfajores`,
-        stock: 5,
-        imagen: `../img/jorgito.png`,
-        cantidad: 0
-    },
-    {
-        id: 2,
-        nombre: `gomitas`,
-        precio: 3000,
-        tipo: `caramelos`,
-        stock: 5,
-        imagen: `../img/gomitasHuesos (1).png`,
-        cantidad: 1
-    },
-    {
-        id: 3,
-        nombre: `fantoche negro`,
-        precio: 700,
-        tipo: `alfajores`,
-        stock: 5,
-        imagen: `../img/fantoche_tripe_negro.png`,
-        cantidad: 0
-    },
-    {
-        id: 4,
-        nombre: `flynn paff`,
-        precio: 2000,
-        tipo: `caramelos`,
-        stock: 3,
-        imagen: `../img/flynnPaff.png`,
-        cantidad: 0
-    },
-    {
-        id: 5,
-        nombre: `pico dulce`,
-        precio: 3800,
-        tipo: `caramelos`,
-        stock: 5,
-        imagen: "../img/picoDulce.png",
-        cantidad: 0
-    }
-] */
-
-const url = `../datos.Json`
-
-fetch()
-
 const CARRITO = JSON.parse(localStorage.getItem(`carrito`)) || [] // En esta variable obtengo el localstorage o sino hay nada inicia en vacio
-
 
 document.getElementById('todos').addEventListener('click',()=>filtado(''))
 document.getElementById('caramelos').addEventListener('click',()=>filtado('caramelos'))
 document.getElementById('alfajores').addEventListener('click',()=>filtado('alfajores'))
 
-mostrar_prod(PRODUCTOS)
+async function obtenerProductos() {
+    const response = await fetch(`../datos.Json`)    
+    const PRODUCTOS = await response.json()
+
+    mostrar_prod(PRODUCTOS)
+}
+
+
+obtenerProductos()
 
 document.addEventListener("DOMContentLoaded", () => { //esta funcion al cargar la pagina muestra los productos, total y contador que habia en el carrito del localstorage
     MostrarCart();
@@ -74,12 +28,17 @@ document.addEventListener("DOMContentLoaded", () => { //esta funcion al cargar l
 //!FUNCTIONS
 
 function filtado(tipo){ //esta funcion filta los productos por determinado tipo 
-    let FILTRADO_PRODUCTOS;
 
-    tipo ?  FILTRADO_PRODUCTOS = PRODUCTOS.filter(producto => producto.tipo === tipo) : FILTRADO_PRODUCTOS = PRODUCTOS
+    fetch(`../datos.Json`)
+        .then(respuesta => respuesta.json())
+        .then(PRODUCTOS => {
+            let FILTRADO_PRODUCTOS;
 
-    mostrar_prod(FILTRADO_PRODUCTOS)
+            tipo ?  FILTRADO_PRODUCTOS = PRODUCTOS.filter(producto => producto.tipo === tipo) : FILTRADO_PRODUCTOS = PRODUCTOS
 
+            mostrar_prod(FILTRADO_PRODUCTOS)
+        })
+        .catch(error => console.log(`Ocurrio un error al filtar los productos.`, error))
     /*
     se usa filter para que encuentre el tipo de producto y los devuelva en un array para poder mostrarlos en pantalla
     sino encuentra el tipo o se preciona el boton "todos" muestra todos los productos
@@ -89,6 +48,7 @@ function filtado(tipo){ //esta funcion filta los productos por determinado tipo
 //!MOSTRAR
 
 function mostrar_prod(FILTRADO_PRODUCTOS){ //la funcion muestra los producto a elegir 
+
     const PRODUCTOS_SECTION = document.getElementById('productos')
     PRODUCTOS_SECTION.innerHTML = '' // limpia la pantalla al iniciar y cuando se elige un tipo de productos
 
@@ -122,47 +82,53 @@ function MostrarCart(){
 
 //!FUNCIONTS CARRITO
 function AgregarCarrito(id){
-    const PRODUCTO = PRODUCTOS.find(producto => producto.id === id); //esta funcion sirve para buscar si se encuentra el producto en el array
 
-    const PROD_CARRITO = CARRITO.find(product => product.id === id) //esta funcion sirve para saber si el producto ya se encuentra en el carrito
+    fetch(`../datos.Json`)
+        .then(respuesta => respuesta.json())
+        .then(PRODUCTOS => {
+            const PRODUCTO = PRODUCTOS.find(producto => producto.id === id); //esta funcion sirve para buscar si se encuentra el producto en el array
 
-    if (PROD_CARRITO) { // si encuentra el producto en el carrito entra al if
-        if (PROD_CARRITO.cantidad < PRODUCTO.stock) {  // si la cantidad es menor al stock entra al if y aumenta la cantidad del producto
-                PROD_CARRITO.cantidad++;
-                Toastify({
-                    text: "Se agrego cantidad",
-                    duration: 1000,
-                    position: "left",
-                    gravity: "top",
-                    style:{
-                        background: "#5b5ba8"
-                    }
-                    }).showToast();
-        } else { // si la cantidad es mayor al stock muestra el mensaje
-            Swal.fire({
-                icon: "error",
-                title: "No Stock",
-                text: "No hay suficiente stock",
-            });
-        }
-    } else { // sino encuentra el prod en el carrito entra al else y agrega el prod al carrito
-            CARRITO.push({ ...PRODUCTO, cantidad: 1 });
-            Toastify({
-                text: "Producto agregado",
-                duration: 1000,
-                position: "right",
-                gravity: "top",
-                style:{
-                    background: "#ADADF3"
+            const PROD_CARRITO = CARRITO.find(product => product.id === id) //esta funcion sirve para saber si el producto ya se encuentra en el carrito
+
+            if (PROD_CARRITO) { // si encuentra el producto en el carrito entra al if
+                if (PROD_CARRITO.cantidad < PRODUCTO.stock) {  // si la cantidad es menor al stock entra al if y aumenta la cantidad del producto
+                        PROD_CARRITO.cantidad++;
+                        Toastify({
+                            text: "Se agrego cantidad",
+                            duration: 1000,
+                            position: "left",
+                            gravity: "top",
+                            style:{
+                                background: "#5b5ba8"
+                            }
+                            }).showToast();
+                } else { // si la cantidad es mayor al stock muestra el mensaje
+                    Swal.fire({
+                        icon: "error",
+                        title: "No Stock",
+                        text: "No hay suficiente stock",
+                    });
                 }
-                }).showToast();
-    }
+            } else { // sino encuentra el prod en el carrito entra al else y agrega el prod al carrito
+                    CARRITO.push({ ...PRODUCTO, cantidad: 1 });
+                    Toastify({
+                        text: "Producto agregado",
+                        duration: 1000,
+                        position: "right",
+                        gravity: "top",
+                        style:{
+                            background: "#ADADF3"
+                        }
+                        }).showToast();
+            }
 
-    localStorage.setItem(`carrito`, JSON.stringify(CARRITO)) // guardo los cambios del carrito en el localstorage
+            localStorage.setItem(`carrito`, JSON.stringify(CARRITO)) // guardo los cambios del carrito en el localstorage
 
-    ActualizarContador();
-    ActualizarTotal();
-    MostrarCart();
+            ActualizarContador();
+            ActualizarTotal();
+            MostrarCart();
+        })
+        .catch(error => console.log(`Ocurrio un error al agregar al carrito`, error))
 }
 
 
